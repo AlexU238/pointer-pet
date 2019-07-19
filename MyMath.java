275ce67070b79cd853651ee74fcb5be7fa88822a
Oleksii <52570864+AlexU238@users.noreply.github.com>
2019-07-19 19:37:32 +0300

    Pet & MyMath updated V2 by @AlexU238

--- MyMath.java
+++ MyMath.java
@@ -1,7 +1,7 @@
 public class MyMath {
     public static final double Pi=3.14;
     public static final double G=9.8;
-    public static final int Two=2;
+    public static final int TWO=2;
 
     public static double subtract(double a, double b){
         return a-b;
@@ -12,10 +12,10 @@ public class MyMath {
     }
 
     public static double multyply(double a){
-        return a*Two;
+        return a*TWO;
     }
 
     public static double divide(double a){
-        return a/Two;
+        return a/TWO;
     }
 }
